09a4fd2d64414dc6a6132fedf543e604e7d87b5b
김형준 <60218554+hyungjun-kim-creator@users.noreply.github.com>
2023-06-30 21:44:25 +0900

    1

---
+++ src/main/java/org/hjun/plugin/allaypoop/AllayPoop.java
@@ -0,0 +1,17 @@
+package org.hjun.plugin.allaypoop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AllayPoop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
